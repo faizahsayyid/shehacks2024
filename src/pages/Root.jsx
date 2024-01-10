62fe153c6112dace0d27be92b6dc391994bb748c
faizahsayyid <faizah.sayyid@gmail.com>
2024-01-10 03:41:22 -0500

    fix: navbar placeholder

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 const Root = () => {
   return (
     <div>
-      Root
+      <NavBar />
       <Outlet />
     </div>
   );
